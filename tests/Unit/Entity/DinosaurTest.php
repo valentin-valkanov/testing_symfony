ed37096bf0016205d40b87f73f8283dfffb5575f
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-08-09 14:06:31 +0300

    Testing Class Methods

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -7,10 +7,19 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testItWorks(): void
+    public function testCanGetAndSetData(): void
     {
-        $dino = new Dinosaur('Rex');
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A',
+        );
 
         $this->assertInstanceOf(Dinosaur::class, $dino);
+        $this->assertSame('Big Eaty', $dino->getName());
+        $this->assertSame('Tyrannosaurus', $dino->getGenus());
+        $this->assertSame(15, $dino->getLength());
+        $this->assertSame('Paddock A', $dino->getEnclosure());
     }
 }
